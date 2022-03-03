c6635b6decdca8560d181dfe4b6d5553ea5df945
JSPRM <albertosp@hotmail.com>
2022-03-03 16:35:09 -0600

    test2

--- myApp.js
+++ myApp.js
@@ -4,12 +4,20 @@ var app = express();
 var send = (req, res) => {
     absolutePath = __dirname + "/views/index.html"
     res.sendFile(absolutePath)
-  };
+};
   
-  app.get("/", send);
+app.get("/", send);
   
   
-  app.use("/public", express.static(__dirname + "/public")); 
+app.use("/public", express.static(__dirname + "/public")); 
+
+app.use("/views/css", express.static(__dirname + "/views/css"));
+app.use("/views/images", express.static(__dirname + "/views/images"));
+app.use("/views/fonts", express.static(__dirname + "/views/fonts"));
+app.use("/views/js", express.static(__dirname + "/views/js"));
+
+
+
 
 
 
