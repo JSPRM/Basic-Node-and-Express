9b6a8032ce6051ad4b9b7757a49a796e10ba1416
JSPRM <albertosp@hotmail.com>
2022-03-03 16:14:09 -0600

    haber

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
 
-
+var send = (req, res) => {
+    absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath)
+  };
+  
+  app.get("/", send);
+  
+  
+  app.use("/public", express.static(__dirname + "/public")); 
 
 
 
