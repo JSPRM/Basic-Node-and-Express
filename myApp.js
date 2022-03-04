de121dfab54d0909ba299dfbc6f57fdf5776533b
JSPRM <albertosp@hotmail.com>
2022-03-04 07:50:38 -0600

    test10

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 var send = (req, res) => {
     absolutePath = __dirname + "/views/index.html"
@@ -11,7 +12,19 @@ app.get("/", send);
   
 app.use("/public", express.static(__dirname + "/public")); 
 
-app.use("/assests/", express.static(__dirname + "/assets/"));
+app.get("/json", (req, res) => {
+    if (MESSAGE_STYLE==="uppercase"){
+        res.json({
+            message: "Hello World".toUpperCase()
+          });
+    }else {
+        res.json({
+            message: "Hello json"
+          });
+    }
+  });
+
+
 
 
 
