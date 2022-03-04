0a96b3a671a330ce7f7ea791f317bd5d4ff9c24e
JSPRM <albertosp@hotmail.com>
2022-03-04 08:06:05 -0600

    test10

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE==="uppercase"){
         res.json({
-            message: "Hello jsom".toUpperCase()
+            message: "Hello json".toUpperCase()
           });
     }else {
         res.json({
